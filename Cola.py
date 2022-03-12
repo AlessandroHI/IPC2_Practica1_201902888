ca18fcde202df0bf13b36d8e62c2013a60f5d636
AlessandroHI <85206440+AlessandroHI@users.noreply.github.com>
2022-03-12 17:04:48 -0600

    Clase Cola

---
+++ Cola.py
@@ -0,0 +1,60 @@
+from Nodo import Nodo
+
+class Cola(Nodo):
+    def __init__(self):
+        super().__init__()
+        self.cabeza = Nodo()
+        self.contador = 0
+        self.valor = self.__str__()
+        
+    def agregar_orden(self, nuevo_nodo):
+        nodo = self.cabeza
+        while(nodo.siguiente):
+            nodo = nodo.siguiente
+        nodo.siguiente = nuevo_nodo
+        self.contador += 1
+        self.valor = self.__str__()
+        
+    def get(self, i):
+        if (i >= self.contador):
+            return None
+        nodo = self.cabeza.siguiente
+        n = 0
+        while(nodo):
+            if (n == i):
+                return nodo
+            nodo = nodo.siguiente
+            n += 1
+
+    def pop(self, i = None):
+        if i is None:
+            i = self.contador - 1
+        if not (0 <= i < self.contador):
+            raise IndexError("Índice fuera de rango")
+        if i == 0:
+            dato = self.cabeza.valor
+            self.cabeza = self.cabeza.siguiente
+        else:
+            n_ant = self.cabeza
+            n_act = n_ant.siguiente
+            for pos in range(1, i):
+                n_ant = n_act
+                n_act = n_ant.siguiente
+            dato = n_act.valor
+            n_ant.siguiente = n_act.siguiente
+        self.contador -= 1
+        return dato
+        
+    def __getitem__(self, i):
+        return self.get(i)
+
+    def length(self):
+        return self.contador
+
+    def entregar_orden(self):
+        self.pop(0)
+
+    def ultimo(self):
+        return self.get(self.length() - 1)
+
+    
